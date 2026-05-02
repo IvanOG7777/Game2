369db789c3a204b05c2507b3486ea5e16c9a6d0d
IvanOG7777 <elderivanargueta04@gmail.com>
2026-05-02 16:45:23 -0700

    INit scene

--- src/Scenes/Init.js
+++ src/Scenes/Init.js
@@ -1,18 +1,21 @@
 class Init extends Phaser.Scene {
     constructor() {
         super("initScene");
+
+        this.my = {text: {}}
+    }
+
+    preload() {
+        this.load.setPath("./assets/"); // loading assets
+        // loading score font
+        this.load.bitmapFont("rocketSquare", "KennyRocketSquare_0.png", "KennyRocketSquare.fnt");
     }
 
      create() {
-        this.add.text(175, 400, "GALLERY SHOOTER", {
-            fontSize: "40px",
-            fill: "#00fd22"
-        });
 
-        this.add.text(220, 500, "Press SPACE to Start", {
-            fontSize: "24px",
-            fill: "#00fd22"
-        });
+        this.my.text.initText = this.add.bitmapText(130, 400,  "rocketSquare", "GALLERY SHOOTER", 40);
+
+        this.add.text(220, 500, "Press SPACE to Start", { fontSize: "24px", fill: "#00fd22"});
 
         this.space = this.input.keyboard.addKey(
             Phaser.Input.Keyboard.KeyCodes.SPACE
